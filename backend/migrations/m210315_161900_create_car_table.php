510f049a81233ae945419487a1968fae88fdaf83
Anton Zheltov <anton6378@gmail.com>
2021-03-16 18:20:38 +0700

    backend: fix image name in migration

--- backend/migrations/m210315_161900_create_car_table.php
+++ backend/migrations/m210315_161900_create_car_table.php
@@ -24,7 +24,7 @@ class m210315_161900_create_car_table extends Migration
         $this->insert("{{%car}}", ['name' => 'BMW 230', 'price' => 3590000, 'picture' => 'BMW 230.png', 'quantity' => 17]);
         $this->insert("{{%car}}", ['name' => 'BMW 330', 'price' => 4125000, 'picture' => 'BMW 330.png', 'quantity' => 1]);
         $this->insert("{{%car}}", ['name' => 'BMW 330e', 'price' => 4455000, 'picture' => 'BMW 330e.png', 'quantity' => 7]);
-        $this->insert("{{%car}}", ['name' => 'BMW 430', 'price' => 4560000, 'picture' => 'BMW 430', 'quantity' => 32]);
+        $this->insert("{{%car}}", ['name' => 'BMW 430', 'price' => 4560000, 'picture' => 'BMW 430.png', 'quantity' => 32]);
         $this->insert("{{%car}}", ['name' => 'BMW 430 Gran Coupe', 'price' => 4475000, 'picture' => 'BMW 430 Gran Coupe.png', 'quantity' => 4]);
         $this->insert("{{%car}}", ['name' => 'BMW 440', 'price' => 5135000, 'picture' => 'BMW 440.png', 'quantity' => 21]);
     }
